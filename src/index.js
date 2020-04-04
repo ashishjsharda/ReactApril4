e9268e124c700c5ac0fb9ef263b519208307a14e
Ashish Sharda <ashishjsharda@gmail.com>
2020-04-04 18:53:55 -0400

    Add all my files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+let user ={
+  firstName:'John',
+  lastName:'Smith'
+};
+
+function show(user)
+{
+  if(user)
+  {
+  return user.firstName + ' ' + user.lastName;
+  }
+  return " Stranger";
+}
+const element= <h1> Hello {show()}</h1>
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    element,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
